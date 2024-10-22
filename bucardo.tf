6b341809328d3dc07d5f7c25d870283e5ac65b5d
Chen <ychchen@80a9973f704f.ctc>
2024-10-22 08:29:47 +0800

    fix

--- bucardo.tf
+++ bucardo.tf
@@ -1,27 +1,27 @@
+provider "aws" {
+  region = "ap-northeast-1"  # 或者您需要的其他区域
+}
+
 module "bucardo_replication" {
   source = "./modules/bucardo"
 
   rds_vpc = {
-    id = "vpc-xxxxxxxxxxxxxxxxx"  # 替换为实际的 VPC ID
-    cidr_block = "10.0.0.0/16"            # 替换为实际的 VPC CIDR 块
-    default_route_table_id = "rtb-xxxxxxxxxxxxxxxxx"  # 替换为实际的默认路由表 ID
+    id = "vpc-0bcc17ca4c39e1496"  # 替换为实际的 VPC ID
+    cidr_block = "172.31.0.0/16"            # 替换为实际的 VPC CIDR 块
+    default_route_table_id = "rtb-019e63e46159130e2"  # 替换为实际的默认路由表 ID
   }
 
-  service_name = "your_service_name"       # 替换为您的服务名称
+  service_name = "testRDS"       # 替换为您的服务名称
   heroku_pg_major_version = "13"                      # 替换为实际的 PostgreSQL 主版本号
 
   rds_instance = {
-    host = "your-rds-endpoint.rds.amazonaws.com"  # 替换为实际的 RDS 端点
-    admin_credentials_arn = "arn:aws:secretsmanager:region:account-id:secret:secret-name"  # 替换为实际的 Secrets Manager ARN
-    security_group_id = "sg-xxxxxxxxxxxxxxxxx"    # 替换为实际的安全组 ID
+    host = "database-1.cluster-c764kouae2wx.ap-northeast-1.rds.amazonaws.com"  # 替换为实际的 RDS 端点
+    admin_credentials_arn = "arn:aws:secretsmanager:ap-northeast-1:034362076319:secret:rds!cluster-2d58a0c3-00ed-47ec-8b76-4e4bfda31edf-7J6TY5"  # 替换为实际的 Secrets Manager ARN
+    security_group_id = "sg-0b86c1c00f05209ad"    # 替换为实际的安全组 ID
   }
 
   environment = "test"
 
-  aws_public_subnet_cidr_block = "10.0.1.0/24"        # 替换为实际的公有子网 CIDR 块
-  aws_public_vpc_cidr_block = "10.0.0.0/16"        # 替换为实际的 VPC CIDR 块
-}
-
-output "debug_file_path" {
-  value = "${path.module}/scripts/setup.sh.tftpl"
+  aws_public_subnet_cidr_block = "172.31.0.0/20"        # 替换为实际的公有子网 CIDR 块
+  aws_public_vpc_cidr_block = "172.31.0.0/16"        # 替换为实际的 VPC CIDR 块
 }
--- modules/bucardo/data.tf
+++ modules/bucardo/data.tf
@@ -2,10 +2,12 @@ data "aws_secretsmanager_secret_version" "rds_admin_credentials" {
   secret_id = var.rds_instance.admin_credentials_arn
 }
 
+// RDS string cred in sm
 data "aws_secretsmanager_secret_version" "rds_credentials" {
-  secret_id = "arn:aws:secretsmanager:[redacted]"
+  secret_id = "arn:aws:secretsmanager:ap-northeast-1:034362076319:secret:asdf-KRFMqU"
 }
 
+// heroku pg string cred in sm
 data "aws_secretsmanager_secret_version" "heroku_database_urls" {
-  secret_id = "arn:aws:secretsmanager:[redacted]"
+  secret_id = "arn:aws:secretsmanager:ap-northeast-1:034362076319:secret:sssss-8vnKDk"
 }
